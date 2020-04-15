519482c6734ac7d2237b1e8785b63dc54d4ea1c3
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-04-15 15:59:09 +1000

    Update description field in setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="waffler",
     version="0.1",
-    description="Description",
+    description="A small program to help make your writing more sophisticated.",
     long_description=open("README.md").read(),
     py_modules=["waffler"],
     install_requires=["click", "nltk"],
